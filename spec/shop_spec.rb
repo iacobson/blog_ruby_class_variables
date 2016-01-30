c04f405c286f0b5adc76567bbeef897b0e5d6405
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2016-01-30 22:11:44 +0000

    Update the test

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -1,20 +1,14 @@
 require_relative '../shop'
 
-describe Fruit do
-  it 'returns the number of fruit categories' do
+describe 'Shop' do
+  it 'returns the number of fruit and vegetable categories' do
     Fruit.new('oranges')
+    Vegetable.new('carrots')
     Fruit.new('bananas')
+    Vegetable.new('cucumbers')
     Fruit.new('oranges')
 
     expect(Fruits.categories).to eq(3)
-  end
-end
-
-describe Vegetable do
-  it 'returns the numer of vegetable categories' do
-    Vegetable.new('carrots')
-    Vegetable.new('cucumbers')
-
     expect(Vegetables.categories).to eq(2)
   end
 end
