d00bd1afbc73659915cfe7b73f477613c60293d0
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2016-01-31 14:26:50 +0000

    fix test file

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -1,14 +1,18 @@
 require_relative '../shop'
 
 describe 'Shop' do
-  it 'returns the number of fruit and vegetable categories' do
+  before(:all) do
     Fruit.new('oranges')
     Vegetable.new('carrots')
     Fruit.new('bananas')
     Vegetable.new('cucumbers')
     Fruit.new('oranges')
-
+  end
+  it 'returns the number of fruit categories' do
     expect(Fruit.categories).to eq(3)
+  end
+
+  it 'returns the number of vegetable categories' do
     expect(Vegetable.categories).to eq(2)
   end
 end
